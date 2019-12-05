106633a65b1f68e8e8e20b1c5abf0a98781dcbcc
gurprit <gssandhu@tekion.com>
2019-12-05 21:42:21 +0530

    fixes route issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes/Routes';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className = "text-center">GST Project</h1>
+      {/* <h1 className = "text-center">GST Project</h1> */}
+      <Routes />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from 'react-router-dom';
 // import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.min.css';
-// import App from './App';
-import LandingPage from '../src/components/landing-page/LandingPage';
+import App from './App';
+// import LandingPage from '../src/components/landing-page/LandingPage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
