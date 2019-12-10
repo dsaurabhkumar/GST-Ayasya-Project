457f4a584e8ce5ac0c7b9fb14a5e33d55a654331
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2019-12-10 12:24:54 +0530

    Code updated

--- src/components/page-not-found/PageNotFound.js
+++ src/components/page-not-found/PageNotFound.js
@@ -3,15 +3,16 @@ import './PageNotFound.css';
 
 function PageNotFound() {
     return (
-        <div className="pageNotFound container">
-            <div>
+        <div className="mainDiv">
+            <div className="pageNotFound container">
                 <h1>404</h1>
-            </div>
-            <div>
-                <h1>Page Not Found !!!</h1>
-                <p>Looks like the page you are looking for is no longer available</p>
+                <div>
+                    <h1>Page Not Found !!!</h1>
+                    <p>Looks like the page you are looking for is no longer available</p>
+                </div>
             </div>
         </div>
+
     )
 }
 export default PageNotFound
