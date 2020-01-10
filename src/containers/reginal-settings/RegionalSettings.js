66715ab963aa8ce7b7ef13ac0e4827ab31c60866
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2020-01-10 17:29:32 +0530

    Regional settings component updated

--- src/containers/reginal-settings/RegionalSettings.js
+++ src/containers/reginal-settings/RegionalSettings.js
@@ -3,9 +3,11 @@ import React from 'react'
 class RegionalSettings extends React.Component {
     render() {
         return(
-            <div>
-                Regional Settings Component
-            </div>
+            <React.Fragment>
+                <div className="container mt-4 mb-4">
+                    <h2 className="text-center">Regional Settings</h2>
+                </div>
+            </React.Fragment>
         )
     }
 }
