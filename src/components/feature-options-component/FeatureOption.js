7a8ef2fd559ee029e1549921c76bf9aecc2deb53
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2020-01-10 14:40:33 +0530

    featureOption component code converted to json

--- src/components/feature-options-component/FeatureOption.js
+++ src/components/feature-options-component/FeatureOption.js
@@ -3,40 +3,70 @@ import '../feature-options-component/FeatureOption.css';
 import { Link } from 'react-router-dom';
 
 class FeatureOption extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            btnText: [
+                {
+                    btnTitle: 'Reginal Settings',
+                    link: '',
+                },
+                {
+                    btnTitle: 'General',
+                    link: '',
+                },
+                {
+                    btnTitle: 'Display Settings',
+                    link: '',
+                },
+                {
+                    btnTitle: 'Accounts',
+                    link: '',
+                },
+                {
+                    btnTitle: 'Inventory',
+                    link: '',
+                },
+                {
+                    btnTitle: 'GST/VAT',
+                    link: '',
+                },
+                {
+                    btnTitle: 'Excise',
+                    link: '',
+                },
+                {
+                    btnTitle: 'Service Tax',
+                    link: '',
+                },
+                {
+                    btnTitle: 'TDS/TCS',
+                    link: '',
+                },
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="mt-3 container">
                 <h3 className="text-center"><span>Feature/Options</span></h3>
                 <p className="text-center mt-3 mb-5">From here you can configure the Features / Options available in Ayasya GST. Please choose the section to be configured from the buttons provided below</p>
                 <div className="featureBtns flex-container mb-4">
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Display Settings</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">General</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Regional Settings</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Accounts</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Inventory</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">GST/VAT</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Excise</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">Service Tax</button>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <button className="optionBtns btn btn-primary">TDS/TCS</button>
-                    </div>
-
+                    {
+                        this.state.btnText.map((val, index) => (
+                            <div key={'btns_' + index} className="col-12 col-md-3">
+                                <div>
+                                {
+                                    val.link.startsWith('http')?
+                                    <a className="optionBtns btn btn-primary" target={val.target} href={val.link}>{val.btnTitle}</a>:
+                                    <Link className="optionBtns btn btn-primary" to={val.link}>{val.btnTitle}</Link>
+                                }
+                                </div>
+                            </div>
+                        ))
+                    }
                     <div className="row btnContainer flex-sm-row-reverse mt-3 optionBtns">
                         <div className="mt-3 col-12 col-md-4 p-0">
                             <button type="submit" className="btn btn-primary optionBtns">Trade Specific Configuration</button>
@@ -49,6 +79,5 @@ class FeatureOption extends React.Component {
             </div>
         )
     }
-
 }
 export default FeatureOption
