6605d99df30ef6087df543a8b6a4d33596c15779
singhianjeetsingh <imanjeet@ayasya.com>
2020-03-26 12:22:45 +0530

    test

--- src/components/nav-bar/NavRelated.js
+++ src/components/nav-bar/NavRelated.js
@@ -1,13 +1,26 @@
 import $ from 'jquery';
 
+// $('.multi-level-dropdown .dropdown-submenu > a').on("mouseenter", function(e) {
+//     var submenu = $(this);
+//     $('.multi-level-dropdown .dropdown-submenu .dropdown-menu').removeClass('show');
+//     submenu.next('.dropdown-menu').addClass('show');
+//     e.stopPropagation();
+// });
+
+// $('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
+//     // hide any open menus when parent closes
+//     $('.multi-level-dropdown .dropdown-menu.show').removeClass('show');
+// });
+
+
 $('.multi-level-dropdown .dropdown-submenu > a').on("mouseenter", function(e) {
     var submenu = $(this);
     $('.multi-level-dropdown .dropdown-submenu .dropdown-menu').removeClass('show');
     submenu.next('.dropdown-menu').addClass('show');
     e.stopPropagation();
-  });
+});
 
-  $('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
+$('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
     // hide any open menus when parent closes
     $('.multi-level-dropdown .dropdown-menu.show').removeClass('show');
-  });
+});
