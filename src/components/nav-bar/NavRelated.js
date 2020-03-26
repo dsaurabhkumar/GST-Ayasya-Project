cb42802600c42b99aa9136da8d230affcd50522e
singhianjeetsingh <dsaurabh.kumar1288@gmail.com>
2020-03-26 13:02:31 +0530

    commented code reverted

--- src/components/nav-bar/NavRelated.js
+++ src/components/nav-bar/NavRelated.js
@@ -1,16 +1,5 @@
 import $ from 'jquery';
 
-// $('.multi-level-dropdown .dropdown-submenu > a').on("mouseenter", function(e) {
-//     var submenu = $(this);
-//     $('.multi-level-dropdown .dropdown-submenu .dropdown-menu').removeClass('show');
-//     submenu.next('.dropdown-menu').addClass('show');
-//     e.stopPropagation();
-// });
-
-// $('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
-//     // hide any open menus when parent closes
-//     $('.multi-level-dropdown .dropdown-menu.show').removeClass('show');
-// });
 
 
 $('.multi-level-dropdown .dropdown-submenu > a').on("mouseenter", function(e) {
