320ca736418e3613ea6712e10ed4c504171db462
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2020-05-15 17:51:28 +0530

    unwanted import removed

--- src/components/date-picker/DatePicker.js
+++ src/components/date-picker/DatePicker.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useFormikContext, Field } from 'formik';
-import moment from 'moment'
 
 
 
